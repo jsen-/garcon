3b54c41c0d5c856acaadd840a55e49a73e4014ac
jsen- <max.enhanced@gmail.com>
2015-06-16 16:49:35 +0200

    use dts-generator

--- dist/garcon.d.ts
+++ dist/garcon.d.ts
@@ -1,28 +1,43 @@
-/// <reference path="../typings/tsd.d.ts" />
-import { Injector, Annotable } from 'stimpack';
-import Context from './Context';
-export { Context };
-export interface RequestHandler {
-    handleRequest(ctx: Context): Promise<any>;
+declare module 'garcon/context' {
+	import * as http from 'http';
+	export default class Context {
+	    req: http.ServerRequest;
+	    res: http.ServerResponse;
+	    constructor(req: http.ServerRequest, res: http.ServerResponse);
+	}
+
 }
-export interface RequestHandlerConstructor extends Annotable {
-    prototype: RequestHandler;
+declare module 'garcon/garcon' {
+	/// <reference path="../typings/tsd.d.ts" />
+	import { Injector, Annotable } from 'stimpack';
+	import Context from './Context';
+	export { Context };
+	export interface RequestHandler {
+	    handleRequest(ctx: Context): Promise<any>;
+	}
+	export interface RequestHandlerConstructor extends Annotable {
+	    prototype: RequestHandler;
+	}
+	export interface Logger {
+	    info(message: string): any;
+	    warn(message: string): any;
+	    error(message: string): any;
+	}
+	export class Garcon {
+	    private injector;
+	    private logger;
+	    private server;
+	    private routes;
+	    constructor(injector: Injector, logger?: Logger);
+	    listen(port: number, hostname?: string, backlog?: number): void;
+	    get(route: string, handler: RequestHandlerConstructor): void;
+	    private handleRequest(req, res);
+	    private info(message);
+	    private warn(message);
+	    private error(message);
+	}
+
 }
-export interface Logger {
-    info(message: string): any;
-    warn(message: string): any;
-    error(message: string): any;
-}
-export declare class Garcon {
-    private injector;
-    private logger;
-    private server;
-    private routes;
-    constructor(injector: Injector, logger?: Logger);
-    listen(port: number, hostname?: string, backlog?: number): void;
-    get(route: string, handler: RequestHandlerConstructor): void;
-    private handleRequest(req, res);
-    private info(message);
-    private warn(message);
-    private error(message);
+declare module 'garcon' {
+	export * from 'garcon/garcon';
 }
